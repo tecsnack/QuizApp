ce89e7369b3a61a173eafae52d039880a6246a1e
RaupeCHR <raupechr@gmail.com>
2023-09-27 11:56:52 +0200

    Repo Quizapp

---
+++ questions.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+      question: 'Was ist die Hauptstadt von Frankreich?',
+      options: ['Paris', 'Berlin', 'Madrid'],
+      correctAnswer: 'Paris',
+    },
+    {
+      question: 'Wie viele Planeten hat unser Sonnensystem?',
+      options: ['7', '8', '9'],
+      correctAnswer: '8',
+    },
+    {
+      question: 'Welches ist das größte Säugetier der Welt?',
+      options: ['Elefant', 'Giraffe', 'Blauwal'],
+      correctAnswer: 'Blauwal',
+    },
+    // Fügen Sie weitere Fragen hinzu
+  ];
+  
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const questions = require('./questions');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/quiz', (req, res) => {
+  if (currentQuestionIndex < questions.length) {
+    res.sendFile(__dirname + '/public/quiz.html');
+  } else {
+    res.send('Quiz beendet');
+  }
+});
+
+app.get('/getQuestion', (req, res) => {
+  if (currentQuestionIndex < questions.length) {
+    res.json(questions[currentQuestionIndex]);
+  } else {
+    res.json({ question: '', options: [] });
+  }
+});
+
+app.post('/submitAnswer', (req, res) => {
+  const userAnswer = req.body.answer;
+  const currentQuestion = questions[currentQuestionIndex];
+
+  if (userAnswer === currentQuestion.correctAnswer) {
+    score++;
+  }
+
+  currentQuestionIndex++;
+
+  res.json({ correct: userAnswer === currentQuestion.correctAnswer, score });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
